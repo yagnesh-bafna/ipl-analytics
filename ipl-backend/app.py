548ba3ad28ff9d99b723027735da246cdded70c0
Yagnesh Bafna <yagnesh@example.com>
2026-03-18 18:34:07 +0530

    fix; removed templete from backend

--- ipl-backend/app.py
+++ ipl-backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
@@ -32,12 +33,7 @@ def add_header(response):
     return response
 
 
-# -----------------------------
-# HOME PAGE (WEB APP)
-# -----------------------------
-@app.route("/")
-def home():
-    return render_template("index.html")
+
 
 
 # -----------------------------
@@ -55,4 +51,5 @@ def api_status():
 # RUN SERVER
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
